afccd8c8978d352010c7f30123a367e6d755c44b
Abid Ali Qureshi <abid.aqureshi@gmail.com>
2025-04-10 21:26:10 +0500

    feat: realtime synchronization implemented

--- frontend/src/types/grid-types.ts
+++ frontend/src/types/grid-types.ts
@@ -1,3 +1,8 @@
 export type GridProps = {
     cells: string[][]; // 2D array of strings representing the grid cells
 };
+
+export type GridGeneratorProps = {
+    grid: string[][]; // 2D array of strings representing the grid cells
+    code_secret: string; // Secret code for the grid
+};
