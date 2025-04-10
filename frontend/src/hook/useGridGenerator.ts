5df02ed57dfb1ba6b22b724fc12d24676ac640e8
Abid Ali Qureshi <abid.aqureshi@gmail.com>
2025-04-10 10:16:41 +0500

    feat: Bias/Weight factor implementation completed

--- frontend/src/hook/useGridGenerator.ts
+++ frontend/src/hook/useGridGenerator.ts
@@ -4,11 +4,14 @@ import { ApiService } from '../service/api/api.service';
 export const useGridGenerator = () => {
     const [grid, setGrid] = useState<string[][]>([]);
     const [codeSecret, setCodeSecret] = useState<string>('');
+    const [biasChar, setBiasChar] = useState<string>('');
     const [isRunning, setIsRunning] = useState<boolean>(false);
 
     const startGenerator = async () => {
         try {
-            const initialGridWithCode = await ApiService.getGridWithCode();
+            const initialGridWithCode = await ApiService.getGridWithCode(
+                biasChar,
+            );
             const { grid, code_secret } = initialGridWithCode;
             setGrid(grid);
             setCodeSecret(code_secret);
@@ -26,6 +29,8 @@ export const useGridGenerator = () => {
         grid,
         codeSecret,
         isRunning,
+        biasChar,
+        setBiasChar,
         startGenerator,
         stopGenerator,
     };
